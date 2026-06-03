311b784f30a21bce4e03e63589be57e0a6b7e0c3
gdinisio <gdinisio@icloud.com>
2026-06-03 15:14:48 +0100

    Initial commit

---
+++ Vault/Vault/VaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  VaultApp.swift
+//  Vault
+//
+//  Created by Giovanni Di Nisio on 03/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct VaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
